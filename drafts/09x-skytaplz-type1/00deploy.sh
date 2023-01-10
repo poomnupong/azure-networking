30d27006a35d017299afa3123db96380479c4e1f
Poom Nupong <poomsawas@gmail.com>
2023-01-10 12:01:32 -0600

    push config-frr, updated

--- drafts/09x-skytaplz-type1/00deploy.sh
+++ drafts/09x-skytaplz-type1/00deploy.sh
@@ -1,9 +1,9 @@
-RGNAME="dev1-skytaplztype1-scus-rg"
+RGNAME="dev2-skytaplztype1-scus-rg"
 REGION="southcentralus"
 az group create --name=$RGNAME --location=$REGION
 az deployment group create \
 --name=deployment1 \
 --resource-group=$RGNAME \
 --template-file="skytaplz-type1.bicep" \
---parameters VM_PASSWORD='Azure123456$' DEPLOY_VPNGW='false' DEPLOY_EXRGW='false' DEPLOY_ROUTESERVER='false'\
+--parameters VM_PASSWORD='Azure123456$' DEPLOY_VPNGW='false' DEPLOY_EXRGW='false' DEPLOY_ROUTESERVER='true'\
 --mode=complete --no-wait
--- drafts/09x-skytaplz-type1/config-frr.sh
+++ drafts/09x-skytaplz-type1/config-frr.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# load apt cache
 sudo apt-get update
 sudo apt-get upgrade -y
+
+# enable ipv4 forwarding
+sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+sudo sysctl -p
 
 # install frr
 sudo apt-get install frr -y
@@ -10,3 +15,13 @@ sudo touch /etc/frr/bgpd.conf
 sudo chown frr /etc/frr/bgpd.conf
 sudo chmod 640 /etc/frr/bgpd.conf
 sudo systemctl enable frr --now
+sudo systemctl restart frr
+
+# configure frr
+sudo bash -c 'cat << EOF > /root/frr-command.txt
+configure
+ip route 10.2.27.192/27 10.2.27.52
+router bgp 65099
+write
+EOF'
+sudo /usr/bin/vtysh -f /root/frr-command.txt
